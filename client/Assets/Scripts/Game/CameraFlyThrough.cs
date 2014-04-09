ca4a659f111d3341f29f9f8856a22bf9493b3f8e
Martin Hollstein <martin.hollstein@foreverinteractive.com>
2014-04-09 13:52:31 -0500

    New art assets added such as fonts and skyboxes. Also added camera scripts to give our menus some livelyhood.

---
+++ client/Assets/Scripts/Game/CameraFlyThrough.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//use this script to fly through gui levels to have the background move and the menu
+//seem more animated.
+
+public class CameraFlyThrough : MonoBehaviour {
+    public float travelSpeed;
+    public float threshHold;
+    public Transform[] paths;
+
+    private int currentIndex = 0;
+    private int numPaths;
+
+	// Use this for initialization
+	void Start () {
+        numPaths = paths.Length;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        iTween.MoveTo(gameObject, paths[currentIndex].position, travelSpeed);
+        iTween.LookTo(gameObject, paths[currentIndex].position, travelSpeed);
+	
+	}
+
+    void FixedUpdate() {
+        if (Vector3.Distance(paths[currentIndex].position, transform.position) <= threshHold) {
+            if (currentIndex < numPaths - 1) {
+                currentIndex++;
+            } else {
+                currentIndex = 0;
+            }
+        }
+    }
+}
---
+++ client/Assets/Scripts/Game/CreditsFaceCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsFaceCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate() {
+        transform.LookAt(Camera.main.transform.position);
+        transform.Rotate(Vector3.up, 180.0f);
+    }
+}
