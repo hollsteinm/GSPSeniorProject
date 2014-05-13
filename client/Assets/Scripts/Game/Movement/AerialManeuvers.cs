abb91772cd8401ac7f0f8952c68e84ce284915ca
kcclubkirby <kcclubkirby@yahoo.com>
2014-05-13 00:14:18 -0700

    Began Aerial Maneuver Script
    In this commit, I started Aerial Maneuvers. Everything is prepared for
    the implementation of maneuvers now. The final step is to fill in the
    "UpdateManeuver." Because it is not filled in, it will not affect the
    regular gameplay right now.

---
+++ client/Assets/Scripts/Game/Movement/AerialManeuvers.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AerialManeuvers : MonoBehaviour {
+	
+	public enum ManeuverType {NONE,LOOP,U_TURN,L_DODGE,R_DODGE};
+	private Transform start,end;
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	public ManeuverType ChooseManeuver(Transform player)
+	{
+		if (Input.GetAxis("Vertical") > 0)
+		{
+			//Save coordinates
+			start = player;
+			end = player;
+			//Return Maneuver Type
+			return ManeuverType.LOOP;
+		}
+		else if (Input.GetAxis("Vertical") < 0)
+		{
+			//Save coordinates
+			start = player;
+			end = player;
+			end.Rotate(-player.forward);
+			//Return Maneuver Type
+			return ManeuverType.U_TURN;
+		}
+		else if (Input.GetAxis("Horizontal") > 0)
+		{
+			//Save coordinates
+			start = player;
+			end = player;
+			end.Translate(-player.right * 25);
+			//Return Maneuver Type
+			return ManeuverType.L_DODGE;
+		}
+		else if (Input.GetAxis("Horizontal") < 0)
+		{
+			//Save coordinates
+			start = player;
+			end = player;
+			end.Translate(player.right * 25);
+			//Return Maneuver Type
+			return ManeuverType.R_DODGE;
+		}
+		else
+			return ManeuverType.NONE;
+	}
+	
+	public bool UpdateManeuver(ManeuverType current, Transform player)
+	{
+		return false;
+	}
+}
--- client/Assets/Scripts/Game/Movement/JetMovement.cs
+++ client/Assets/Scripts/Game/Movement/JetMovement.cs
@@ -24,6 +24,11 @@ public class JetMovement : MonoBehaviour {
 	private float horizontalVelocity = 0.0f;
 	public float horizontalVelocityDeviation = 20.0f;
 	
+	//Used for aerial maneuvers
+	private bool usingManeuver = false;
+	private AerialManeuvers.ManeuverType currentManeuver;
+	private AerialManeuvers maneuver = new AerialManeuvers();
+	
 	// Use this for initialization
 	void Start () {
 		forwardVelocity = defaultForwardVelocity;
@@ -32,90 +37,103 @@ public class JetMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-		//SHIP ROTATION
-		
-		float rotX = Input.GetAxis("Mouse X") * lookSpeed;
-        float rotY = Input.GetAxis("Mouse Y") * lookSpeed;
-		
-		transform.Rotate (Vector3.up * rotX);
-		transform.Rotate (Vector3.left * rotY);
-		
-		//FORWARD THRUSTERS
-		
-		//If we are not boosting or braking, we should return to the
-		//standard velocity by flipping the acceleration
-		float thrust = Input.GetAxis("Vertical") * forwardAccelerationReaction;
-		if (thrust == 0 && forwardAcceleration != 0)
-		{
-			if (forwardVelocity > defaultForwardVelocity)
-				forwardAcceleration = -maxForwardAcceleration;
-			else if (forwardVelocity < defaultForwardVelocity)
-				forwardAcceleration = maxForwardAcceleration;
-			else
-				forwardAcceleration = 0;
-		}
-		//Otherwise we should increase the acceleration by the thrust amount
-        forwardAcceleration += thrust;
-        forwardAcceleration = Mathf.Clamp(forwardAcceleration, -maxForwardAcceleration, maxForwardAcceleration);
-		
-		//If we are not boosting or braking, we should check to see if the
-		//velocity is going to reach the default velocity
-		float deltaVelocity = forwardAcceleration * Time.deltaTime;
-		if(thrust == 0)
-		{
-			if((forwardVelocity > defaultForwardVelocity && forwardVelocity + deltaVelocity < defaultForwardVelocity) ||
-			   (forwardVelocity < defaultForwardVelocity && forwardVelocity + deltaVelocity > defaultForwardVelocity))
-			{
-				//If so, just change the values accordingly
-				forwardVelocity = defaultForwardVelocity;
-				deltaVelocity = 0;
-			}				
-		}
-        forwardVelocity += deltaVelocity;
-		
-		//Make sure the velocity does not exceed the max or min values
-		forwardVelocity = Mathf.Clamp(forwardVelocity,
-							   defaultForwardVelocity - forwardVelocityDeviation,
-			                   defaultForwardVelocity + forwardVelocityDeviation);
-		
-		//SIDE THRUSTERS
-		
-		//If we are not using the side thrusters, we should return to zero
-		//horizontal velocity by flipping the acceleration
-		thrust = Input.GetAxis("Horizontal") * horizontalAccelerationReaction;
-		if (thrust == 0 && horizontalAcceleration != 0)
-		{
-			if (horizontalVelocity > 0)
-				horizontalAcceleration = -maxHorizontalAcceleration;
-			else if (horizontalVelocity < 0)
-				horizontalAcceleration = maxHorizontalAcceleration;
-			else
-				horizontalAcceleration = 0;
-		}
-		//Otherwise we should increase the acceleration by the thrust amount
-        horizontalAcceleration += thrust;
-        horizontalAcceleration = Mathf.Clamp(horizontalAcceleration, -maxHorizontalAcceleration, maxHorizontalAcceleration);
-		
-		//If we are not using the side thrusters, we should check to see if the
-		//velocity is going to reach zero
-		deltaVelocity = horizontalAcceleration * Time.deltaTime;
-		if(thrust == 0)
-		{
-			if((horizontalVelocity > 0 && horizontalVelocity + deltaVelocity < 0) ||
-			   (horizontalVelocity < 0 && horizontalVelocity + deltaVelocity > 0))
-			{
-				//If so, just change the values accordingly
-				horizontalVelocity = 0;
-				deltaVelocity = 0;
-			}				
-		}
-        horizontalVelocity += deltaVelocity;
-		
-		//Make sure the velocity does not exceed the max or min values
-		horizontalVelocity = Mathf.Clamp(horizontalVelocity, -horizontalVelocityDeviation, horizontalVelocityDeviation);
 
-        transform.position += transform.forward * forwardVelocity * Time.deltaTime;
-        transform.position += transform.right * horizontalVelocity * Time.deltaTime;
+		if (Input.GetMouseButtonDown(1))
+		{
+			currentManeuver = maneuver.ChooseManeuver(transform);
+			if (currentManeuver != AerialManeuvers.ManeuverType.NONE)
+				usingManeuver = true;
+		}
+		if (usingManeuver)
+		{
+			usingManeuver = maneuver.UpdateManeuver(currentManeuver,transform);
+		}
+		else
+		{
+			//SHIP ROTATION
+			
+			float rotX = Input.GetAxis("Mouse X") * lookSpeed;
+	        float rotY = Input.GetAxis("Mouse Y") * lookSpeed;
+			
+			transform.Rotate (Vector3.up * rotX);
+			transform.Rotate (Vector3.left * rotY);
+			
+			//FORWARD THRUSTERS
+			
+			//If we are not boosting or braking, we should return to the
+			//standard velocity by flipping the acceleration
+			float thrust = Input.GetAxis("Vertical") * forwardAccelerationReaction;
+			if (thrust == 0 && forwardAcceleration != 0)
+			{
+				if (forwardVelocity > defaultForwardVelocity)
+					forwardAcceleration = -maxForwardAcceleration;
+				else if (forwardVelocity < defaultForwardVelocity)
+					forwardAcceleration = maxForwardAcceleration;
+				else
+					forwardAcceleration = 0;
+			}
+			//Otherwise we should increase the acceleration by the thrust amount
+	        forwardAcceleration += thrust;
+	        forwardAcceleration = Mathf.Clamp(forwardAcceleration, -maxForwardAcceleration, maxForwardAcceleration);
+			
+			//If we are not boosting or braking, we should check to see if the
+			//velocity is going to reach the default velocity
+			float deltaVelocity = forwardAcceleration * Time.deltaTime;
+			if(thrust == 0)
+			{
+				if((forwardVelocity > defaultForwardVelocity && forwardVelocity + deltaVelocity < defaultForwardVelocity) ||
+				   (forwardVelocity < defaultForwardVelocity && forwardVelocity + deltaVelocity > defaultForwardVelocity))
+				{
+					//If so, just change the values accordingly
+					forwardVelocity = defaultForwardVelocity;
+					deltaVelocity = 0;
+				}				
+			}
+	        forwardVelocity += deltaVelocity;
+			
+			//Make sure the velocity does not exceed the max or min values
+			forwardVelocity = Mathf.Clamp(forwardVelocity,
+								   defaultForwardVelocity - forwardVelocityDeviation,
+				                   defaultForwardVelocity + forwardVelocityDeviation);
+			
+			//SIDE THRUSTERS
+			
+			//If we are not using the side thrusters, we should return to zero
+			//horizontal velocity by flipping the acceleration
+			thrust = Input.GetAxis("Horizontal") * horizontalAccelerationReaction;
+			if (thrust == 0 && horizontalAcceleration != 0)
+			{
+				if (horizontalVelocity > 0)
+					horizontalAcceleration = -maxHorizontalAcceleration;
+				else if (horizontalVelocity < 0)
+					horizontalAcceleration = maxHorizontalAcceleration;
+				else
+					horizontalAcceleration = 0;
+			}
+			//Otherwise we should increase the acceleration by the thrust amount
+	        horizontalAcceleration += thrust;
+	        horizontalAcceleration = Mathf.Clamp(horizontalAcceleration, -maxHorizontalAcceleration, maxHorizontalAcceleration);
+			
+			//If we are not using the side thrusters, we should check to see if the
+			//velocity is going to reach zero
+			deltaVelocity = horizontalAcceleration * Time.deltaTime;
+			if(thrust == 0)
+			{
+				if((horizontalVelocity > 0 && horizontalVelocity + deltaVelocity < 0) ||
+				   (horizontalVelocity < 0 && horizontalVelocity + deltaVelocity > 0))
+				{
+					//If so, just change the values accordingly
+					horizontalVelocity = 0;
+					deltaVelocity = 0;
+				}				
+			}
+	        horizontalVelocity += deltaVelocity;
+			
+			//Make sure the velocity does not exceed the max or min values
+			horizontalVelocity = Mathf.Clamp(horizontalVelocity, -horizontalVelocityDeviation, horizontalVelocityDeviation);
+	
+	        transform.position += transform.forward * forwardVelocity * Time.deltaTime;
+	        transform.position += transform.right * horizontalVelocity * Time.deltaTime;
+		}
 	}
 }
