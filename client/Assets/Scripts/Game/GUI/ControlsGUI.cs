cabaa99078a55fe44e3d49469c69733ce3aa3b9b
Martin Hollstein <martin.hollstein@foreverinteractive.com>
2014-05-19 22:10:44 -0500

    Added menu option to see controls.

---
+++ client/Assets/Scripts/Game/GUI/ControlsGUI.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlsGUI : MonoBehaviour {
+    public GUIStyle controlsStyle;
+    public GUIStyle backStyle;
+
+    public GUIContent backContent;
+    public Rect backRect;
+
+    public int x;
+    public int y;
+    public int width;
+    public int height;
+    public int xstride;
+    public int ystride;
+
+    public string[] controlsList;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI() {
+        if (GUI.Button(backRect, backContent, backStyle)) {
+            Application.LoadLevel("launch");
+        }
+
+        int index = 1;
+        foreach (string s in controlsList){
+            GUI.Label(new Rect(x + (xstride * index), y + (ystride * index), width, height), s, controlsStyle);
+            index++;
+        } 
+    }
+}
--- client/Assets/Scripts/Game/GUI/LaunchGUI.cs
+++ client/Assets/Scripts/Game/GUI/LaunchGUI.cs
@@ -6,6 +6,7 @@ public class LaunchGUI : MonoBehaviour {
     public Rect multiPlayerRect;
     public Rect creditsRect;
     public Rect quitRect;
+    public Rect controlsRect;
 
     public GUIStyle launchStyle;
 
@@ -13,6 +14,7 @@ public class LaunchGUI : MonoBehaviour {
     public GUIContent multiPlayerContent;
     public GUIContent creditContent;
     public GUIContent quitContent;
+    public GUIContent controlsContent;
 
     public float groupWidth;
     public float groupHeight;
@@ -44,6 +46,10 @@ public class LaunchGUI : MonoBehaviour {
         if (GUI.Button(creditsRect, creditContent, launchStyle)) {
             GameManager.gameManager.gameType = GameType.SINGLEPLAYER;
             Application.LoadLevel("credits");
+        }
+
+        if (GUI.Button(controlsRect, controlsContent, launchStyle)) {
+            Application.LoadLevel("controls");
         }
 
         if (GUI.Button(quitRect, quitContent, launchStyle)) {
