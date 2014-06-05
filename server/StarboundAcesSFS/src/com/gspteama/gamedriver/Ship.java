907727ad2843cf91a97d7f0cbaac5247aeec8a35
hollsteinm <moonshine_122@hotmail.com>
2014-06-05 13:11:07 -0500

    Update Ship.java
    more property updates

--- server/StarboundAcesSFS/src/com/gspteama/gamedriver/Ship.java
+++ server/StarboundAcesSFS/src/com/gspteama/gamedriver/Ship.java
@@ -11,14 +11,9 @@ import java.util.ArrayList;
  * @author Martin
  */
 public class Ship {
-    private float       position[]  = new float[]{0.0f, 0.0f, 0.0f};
-    private float       rotation[]  = new float[]{0.0f, 0.0f, 0.0f, 0.0f};
-    private float       eulerRotation[] = new float[]{0.0f, 0.0f, 0.0f};
-    
+    public Movement     movement;
     public float        velocity = 0.0f;
     public float        acceleration = 0.0f;
-    
-    protected static final float       maxVelocity = 1000.0f;
     
     private long                shipTypeID = 0;
     private String              shipTypeString = "";
@@ -26,32 +21,19 @@ public class Ship {
     protected Weapon            weapon;
     protected Hull              hull;
     
-    private long                otherShipIDIamCollidingWith;
-    private long                otherProjectileIamCollidingWith;
+    private long                owningPlayerID;
     
-    public Ship(long shipTypeID, String shipTypeString, Weapon weapon, Hull hull){
-        weapon = new Weapon();
-        position = new float[3];
-        rotation = new float[4];
-        eulerRotation = new float [3];
-        this.health = health;
+    public Ship(long owningPlayerID, long playerId, long shipTypeID, String shipTypeString, Weapon weapon, Hull hull, float maxVelocity){
+        movement = new Movement(maxVelocity);
+        this.hull = hull;
         this.weapon = weapon;
+        this.shipTypeID = shipTypeID;
+        this.shipTypeString = shipTypeString;
+        this.owningPlayerID = owningPlayerID;
     }
     
-    public boolean verifyCollisionShip(long otherShipID){
-        return otherShipID == otherShipIDIamCollidingWith;
-    }
-    
-    public boolean verifyCollisionProjectile(long projectileID){
-        return otherShipID == projectileID;
-    }
-    
-    public void setCollisionHandshakeShip(long otherShipID){
-        otherSHipIDIamCollidingWith = otherShipIDIamCollidingWith;
-    }
-    
-    public void setCollisionHandshakeProjectile(long otherProjectileID){
-        otherProjectileIamCollidingWith = otherProjectileID;
+    public long getOwningPlayerID(){
+        return owningPlayerID;
     }
     
     public long getTypeID(){
@@ -60,42 +42,6 @@ public class Ship {
     
     public String getShipTypeString(){
         return shipTypeString;
-    }
-
-    public float[] getPosition() {
-        return position;
-    }
-
-    public void setPosition(float[] position) {
-        this.position = position;
-    }
-
-    public float[] getRotation() {
-        return rotation;
-    }
-    
-    public float[] getEulerRotation(){
-        return eulerRotation;
-    }
-
-    public void setRotation(float[] rotation) {
-        this.rotation = rotation;
-    }
-    
-    public void setEulerRotation(float[] eulerRotation){
-        this.eulerRotation = eulerRotation;
-    }
-    
-    public float setPosition(float x, float y, float z){
-        this.position = new float[]{x, y, z};
-    }
-    
-    public float setRotation(float x, float y, float z, float w){
-        this.rotation = new float[]{x, y, z, w};
-    }
-    
-    public float setEulerRotation(float x, float y, float z){
-        this.eulerRotation = new float[]{x, y, z};
     }
 
     public Hull getHull(){
