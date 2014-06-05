c0970f8e395602d804a935b845592b2df2acc125
hollsteinm <moonshine_122@hotmail.com>
2014-06-05 16:30:22 -0500

    Update Weapon.java

--- server/StarboundAcesSFS/src/com/gspteama/gamedriver/Weapon.java
+++ server/StarboundAcesSFS/src/com/gspteama/gamedriver/Weapon.java
@@ -12,11 +12,18 @@ public class Weapon {
     protected float       cooldown    = 0.0f;
     protected Projectile  projectile;
     
+    private int maxClipSize = 0;
+    private int totalAmmo = 0;
+    
     private float         currentCooldown = 0.0f;
     
-    public Weapon(float cooldown, Projectile projectile){
+    private int currentAmmo = 0;
+    
+    public Weapon(float cooldown, Projectile projectile, int maxClipSize, int totalAmmo){
         this.cooldown = cooldown;
         this.projectile = projectile;
+        this.maxClipSize = maxClipSize;
+        this.totalAmmo = totalAmmo;
     }
 
     public float getCooldown() {
@@ -27,13 +34,30 @@ public class Weapon {
         currentCooldown -= value;
     }
     
+    public void onReload(){
+        if(canReload()){
+            if(totalAmmo > (maxClipSize - currentAmmo){
+                totalAmmo -= (maxClipSize - currentAmmo)
+                currentAmmo = maxClipSize;
+            } else {
+                currentAmmo += totalAmmo;
+                totalAmmo = 0;
+            }
+        }
+    }
+    
+    private bool canReload(){
+        return currentAmmo < maxClipSize && totalAmmo > 0;
+    }
+    
     public bool canFire(){
-        return currentCooldown <= 0.0f;
+        return currentCooldown <= 0.0f && currentAmmo > 0;
     }
     
     public Projectile onFire(){
         currentCooldown = cooldown;
-        return projectile;
+        currentAmmo--;
+        return new Projectile(projectile);
     }
     
 }
